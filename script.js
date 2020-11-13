9880b8ca712cfeb9d0a2df11de55a28cfa049541
wahyuamirulloh <amirullohw@gmail.com>
2020-11-13 22:11:43 +0700

    alpha tester without style

--- script.js
+++ script.js
@@ -1,4 +1,16 @@
 let ask_number = prompt('Masukkan angka 1 - 11 untuk menentukan tingkat kesulitan');
+if (ask_number > 11){
+    alert('Anda memasukkan lebih dari 11, merubah masukkan anda ke maksimum, 11');
+    ask_number = 11;
+} else if (ask_number < 2) {
+    alert('Anda memasukkan kurang dari 2, merubah masukkan anda ke minimum, 2');
+    ask_number = 2;
+} else if (ask_number >= 2 && ask_number <= 11) {
+    alert('Permainan dimulai');
+} else {
+    alert('Anda tidak memasukkan angka, merubah masukkan anda ke default, 5');
+    ask_number = 5;
+}
 
 let fruits = ['banana', 'banana', 'black-cherry', 'black-cherry', 'coconut', 'coconut', 'green-apple', 'green-apple', 'orange', 'orange',  'peach', 'peach', 'raspberry', 'raspberry', 'red-cherry', 'red-cherry', 'star-fruit', 'star-fruit', 'strawberry', 'strawberry', 'watermelon', 'watermelon'];
 let fruit_store = new Array();
@@ -38,9 +50,11 @@ for (i = fruit_store.length - 1; i >= 0; i--) {
     let card = document.getElementById(`card-${i}`);
     let card_image = card.firstChild;
     var card_image_first;
+    var card_body_first;
     card.addEventListener('click', function() {
         card_picked.push(card_image.name);
         card_image.classList.toggle('hide');
+        card.classList.toggle('card-show');
         if(card_image.classList.length == 0){
             if (card_picked.length == 2){
                 if(card_picked[0] == card_picked[1]){
@@ -48,6 +62,7 @@ for (i = fruit_store.length - 1; i >= 0; i--) {
                     console.log('Sama');
                     card_image.classList.remove('hide');
                     card_image.classList.add('show-fixed');
+                    card.classList.add('card-show-fixed');
                     card_stored.push(card_picked);
                     console.log(card_stored);
                     card_picked = [];
@@ -56,11 +71,14 @@ for (i = fruit_store.length - 1; i >= 0; i--) {
                     card_picked = [];
                     setTimeout(() => { 
                         card_image.classList.toggle('hide');
-                        card_image_first.classList.toggle('hide')
-                    },500);
+                        card_image_first.classList.toggle('hide');
+                        card_body_first.classList.remove('card-show');
+                        card.classList.remove('card-show');
+                    },300);
                 }
             } else {
                 card_image_first = card_image;
+                card_body_first = card;
             }
             console.log(card_picked);
         } else {
